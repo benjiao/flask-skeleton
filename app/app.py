b4b6215aa721862e7e13ffa914dd0a289ffdbb75
Benjie Jiao <benjiao12@gmail.com>
2014-11-20 02:30:52 +0800

    Initial commit with code.

---
+++ app/app.py
@@ -0,0 +1,13 @@
+#!flask/bin/python
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', header="Basic Python App")
+
+if __name__ == '__main__':
+    app.run(debug=True)
