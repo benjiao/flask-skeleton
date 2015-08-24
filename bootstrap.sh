6be21bad648bf297f7044a236206b51027d8231a
Benjie Jiao <benjiao12@gmail.com>
2015-08-25 03:05:07 +0800

    Added nginx config setup script in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,3 +15,8 @@ touch /var/log/uwsgi.${APPNAME}.log
 chmod 777 /var/log/uwsgi.${APPNAME}.logi
 chmod 777 /var/log/uwsgi.${APPNAME}.log
 
+
+# Setup nginx config
+# Note: To select which port this app should listen to, edit nginx.example
+cp -Rpv nginx.example /etc/nginx/sites-available/${APPNAME}
+ln -s /etc/nginx/sites-available/${APPNAME} /etc/nginx/sites-enabled/${APPNAME}
